677c1e688b89b5108771493caabc89c4949fd1d6
cmorrison <cmorrison@tsys.com>
2022-12-24 14:54:32 -0500

    Training

---
+++ src/main/java/com/demo/jpa2/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.demo.jpa2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(ResponseStatusException.class)
+  public ResponseEntity<Object> handleResponseStatusException(
+      ResponseStatusException responseStatusException,
+      WebRequest request) {
+    return null;
+  }
+
+}
